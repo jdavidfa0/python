0ce4ca050677fcac88b0048681db57b54150b4a3
Jdavidfa0 <jdavidfa0@gmail.com>
2024-02-06 09:45:04 -0500

    Alturas

---
+++ Alturas.py
@@ -0,0 +1,12 @@
+cont =0
+acum =0
+alturas =int(input("Ingrese cantidad de alturas"))
+while cont<alturas:
+    num= int(input("Ingrese altura:  "))
+    acum=acum+num
+
+    cont+= 1
+Promedio = acum/alturas
+
+print ("La suma de alturas es: ", acum)
+print ("El promedio de alturas es: ",Promedio)
