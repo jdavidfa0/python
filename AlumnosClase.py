858051ae2d7c460510bf5ed34902c67d2495789e
Jdavidfa0 <jdavidfa0@gmail.com>
2024-02-13 09:47:04 -0500

    Pythonn

--- AlumnosClase.py
+++ AlumnosClase.py
@@ -1,21 +1,27 @@
-def clase():
-    alumnos={
 
-    }
+alumnos={}
 
-cant=int(input("Ingrese cantidad de alumnos: "))
-    
+cant = int(input("Ingrese cantidad de alumnos: "))
+
 for x in range(cant):
-    nombre=input("Ingrese el nombre del alumno:  ")
-    Notas=[]
-    nota=float(input("Ingrese la nota del alumno: "))
-    Notas.append(nota)
-    while nota>=0:
-        nota=float(input("Ingrese la nota del alumno: "))
-        Notas.append(nota)
+    nombre = input("Ingrese el nombre del alumno:  ")
+    nota=0
+   
+    Notas = []
+    while nota >= 0:
+     nota = float(input("Ingrese la nota del alumno: "))
+     Notas.append(nota)
+
+     alumnos[nombre]=Notas
+
+for nombre in alumnos:
+   print(nombre)
+   sum=0
+   for nota in alumnos[nombre]:
+      
+      sum+=nota
+
+      print(nombre,sum)
 
 
-
-
-
-
+   
